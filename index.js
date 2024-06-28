17d6beda813b92396230d113f12f501c20eb2f69
stephenmwandau <stephen.mwandau@student.moringaschool.com>
2024-06-28 14:38:19 +0300

    arrays

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+destructivelyAppendCat = function (name) {
+    cats.push(name);
+};
+destructivelyPrependCat = function (name) {
+    cats.unshift(name);
+};
+destructivelyRemoveLastCat = function () {
+    cats.pop();
+};
+destructivelyRemoveFirstCat = function () {
+    cats.shift();
+};
+appendCat = function (name) {
+    return [...cats, name];
+};
+removeLastCat = function () {
+    return cats.slice(0, -1);
+};
+prependCat = function (name) {
+    return [name, ...cats];
+};
+appendCat = function (name) {
+    return [...cats, name];
+};
+removeFirstCat = function () {
+    return cats.slice(1);
+}
